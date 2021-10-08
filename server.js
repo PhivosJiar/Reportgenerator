c5b5db020b14f7c2d26b7f25627c4ffc96613b04
PhivosJiar <paulyuancho@gmail.com>
2021-10-08 15:18:57 +0800

    調整 server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/report-generator-from-angular'));
+app.use(express.static('./dist/GraffitiWall'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/report-generator-from-angular/'}),
+    res.sendFile('index.html', {root: 'dist/GraffitiWall/'}),
 );
 
 // Start the app by listening on the default Heroku port
